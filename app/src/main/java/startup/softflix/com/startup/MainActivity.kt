8ac1849c1552da7ecde69e752a261beb8055148f
Zanis Ali <zanisali999@gmail.com>
2018-12-16 13:15:55 +0100

    final commit

--- app/src/main/java/startup/softflix/com/startup/MainActivity.kt
+++ app/src/main/java/startup/softflix/com/startup/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -18,20 +20,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
     }
 
-    protected fun GetSunset(view: View)
-    {
+
+      fun GetSunset(view: View)
+      {
+          //toast is to check that function is being called or not
+          Toast.makeText(this,"Checking", Toast.LENGTH_LONG)
         var city=etCityName.toString()
-        val url="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2Cny%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        val url="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+ city +"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
         //this will directly fire function of asynch class that is do in bg
         MyAsynchTask().execute(url)
     }
 
+
+
     //sending httprequest and getting data will take alot of time and app can crash so use thread or asynch class
 
     //3 instances can send but do in bg is using first instance
-    inner class MyAsynchTask:AsyncTask<String, String, String>
+    inner class MyAsynchTask: AsyncTask<String, String, String>()
     {
         override fun onPreExecute() {
             //will be executed before task started
@@ -51,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                 publishProgress(inString)
             }catch (ex:Exception){}
 
-            return null!!
+            return " "
 
         }
 
@@ -62,6 +70,9 @@ class MainActivity : AppCompatActivity() {
                 val results= query.getJSONObject("results")
                 val channel= results.getJSONObject("channel")
                 val astronomy= channel.getJSONObject("astronomy")
+                val sunrise=astronomy.getString("sunrise")
+                //showing it to ui now
+                tvSunSetTime.text="Sunrise time is "+ sunrise
 
             }catch (ex:Exception){}
         }
@@ -79,16 +90,17 @@ class MainActivity : AppCompatActivity() {
 
         try {
             do {
-                line=bufferReader.readLine()
-                if(line!=null)
-                {
-                    AllString+=line
+                line = bufferReader.readLine()
+                if (line != null) {
+                    AllString += line
 
                 }
-                while(line!=null)
-                    inputStream.close()
+
 
             }
+
+            while (line != null)
+            inputStream.close()
         }catch (ex:Exception){}
 
         return AllString
